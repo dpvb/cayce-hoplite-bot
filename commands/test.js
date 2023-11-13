18cdb335b70d00c2b1e4e52cb68ef26e29d6f3e1
dbunnik3 <dbunnik3@gatech.edu>
2023-11-13 16:01:28 -0500

    First commit.

---
+++ commands/test.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('test')
+		.setDescription('Test command!'),
+	async execute(interaction) {
+		await interaction.reply('Test message.');
+	},
+};
